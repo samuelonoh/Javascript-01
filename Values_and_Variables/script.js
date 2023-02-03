49d00d3923bf922e3928ac71ac2fed5ded64b6cb
samuelonoh <samuelonoh12345@gmail.com>
2023-02-03 15:39:57 +0100

    Variables

---
+++ Values_and_Variables/script.js
@@ -0,0 +1,41 @@
+// VALUES AND VARIABLES
+// Variable is a box, where we can store a value . It can be issued a name.
+
+// RULES FOR VARIABLES
+// Names can contain letters, digits, _ and $
+// Names cannot start with a digit
+// Names can start with an _ or $
+// Names are case sensitive
+// Names cannot be reseved keywords
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+// Carmel Case
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Snake Case
+let jonas_maltida = "JM";
+
+// Pascal Case
+let NameOfFruit = "Orange"
+console.log(NameOfFruit);
+
+let $function = 27;
+
+// constant
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+
+
+console.log(myFirstJob);
